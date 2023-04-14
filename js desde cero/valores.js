da47f1daddc10429720b779fe8b598e89655a41a
Daniel Lujan <daniel.lujan.cba@gmail.com>
2023-04-13 22:10:34 -0300

    aqui comenzamos

---
+++ js desde cero/valores.js
@@ -0,0 +1,20 @@
+// valores
+
+// string/ puede usar comilla doble o simple
+"daniel"
+'roberto'
+
+// enteros
+4
+
+//flotantes
+4.4
+
+//valores booleanos
+true // verdadero
+false // falso
+
+// valores vacios
+null
+undefined
+
---
+++ python_ejercicios/listas.py
@@ -0,0 +1,7 @@
+lista1 = ["lunes","martes","miercoles","jueves", "viernes"]
+
+lista2 = ["sabado","domingo"]
+
+lista3 = lista1 + lista2
+
+print(lista3)
