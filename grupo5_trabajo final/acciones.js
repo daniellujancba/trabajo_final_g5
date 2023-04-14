7d07730d99189912f841a964467a074a5cbdb4c5
Daniel Lujan <daniel.lujan.cba@gmail.com>
2023-04-14 16:00:15 -0300

    se agrego el funcionamento de los btn

--- grupo5_trabajo final/acciones.js
+++ grupo5_trabajo final/acciones.js
@@ -1,1 +1,17 @@
 /* capturar los botones*/
+const form1 = document.getElementById("formcontactos_1");
+const form2 = document.getElementById("formcontactos_2");
+
+
+const btnForm=document.getElementsByClassName("btn-form");
+console.log(btnForm);
+
+for(let i=0 ; i<btnForm.length ; i++){
+    btnForm[i].addEventListener("click", secuencia);
+}
+
+function secuencia(){
+    form1.classList.toggle("d-none");
+    form2.classList.toggle("d-none");
+
+}
